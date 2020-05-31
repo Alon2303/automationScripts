3bea5f2a7a4ee7d2b2601b7a2f68dfa75575e0e9
Alon Ofir <alonof27@gmail.com>
2020-05-31 23:18:14 +0300

    Updated Commits

--- autoUpdateFromGit.js
+++ autoUpdateFromGit.js
@@ -7,14 +7,14 @@ const time = 1000 * 60 * 15; // 15 min
 const updateProject = () => {
 	console.log(new Date());
 	
-	// UPDATE BackEnd
+	// UPDATE BackEnd --> B/E repo
 	child_process.exec('frontendFile.bat', function(error, stdout, stderr) {
     if(error)
 		console.error(error);
 	console.log(stdout);
 	});
 	
-	// UPDATE FronEnd
+	// UPDATE FronEnd --> F/E
 	child_process.exec('backendFIle.bat', function(error, stdout, stderr) {
     if(error)
 		console.error(error);
