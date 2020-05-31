693d3fd32fc242b92159ebb7771d6f65c7e77797
Alon Ofir <alonof27@gmail.com>
2020-05-31 23:16:25 +0300

    Create autoUpdateFromGit.js

---
+++ autoUpdateFromGit.js
@@ -0,0 +1,30 @@
+var child_process = require('child_process');
+
+console.log('autoUpadte');
+
+const time = 1000 * 60 * 15; // 15 min
+
+const updateProject = () => {
+	console.log(new Date());
+	
+	// UPDATE BackEnd
+	child_process.exec('frontendFile.bat', function(error, stdout, stderr) {
+    if(error)
+		console.error(error);
+	console.log(stdout);
+	});
+	
+	// UPDATE FronEnd
+	child_process.exec('backendFIle.bat', function(error, stdout, stderr) {
+    if(error)
+		console.error(error);
+	console.log(stdout);
+	});
+	
+}
+
+updateProject();
+
+const interval = setInterval(()=>{
+	updateProject();	
+},time)
