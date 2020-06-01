dd12ad74aef17565fd0f6e52fd810f74e277f19c
Alon Ofir <alonof27@gmail.com>
2020-06-01 13:24:33 +0300

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,8 @@
+title someTitle
+rem fuplicate this file for any other project --> just add it to the auto update
+rem path to your repo root
+c:
+rem CD to repo folder
+cd c://some/folder
+rem run git pull
+git pull
